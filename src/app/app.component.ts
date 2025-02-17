13988d58d1bbfcd41e8bccb3bd09cd82551ac2d1
gtarrojo <175803244+gtarrojo@users.noreply.github.com>
2025-02-17 15:19:39 +0100

    add components structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { FormularioBlogComponent } from './components/formulario-blog/formulario-blog.component';
+import { ListadoBlogComponent } from './components/listado-blog/listado-blog.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormularioBlogComponent, ListadoBlogComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'angularActBlog';
-}
+export class AppComponent {}
