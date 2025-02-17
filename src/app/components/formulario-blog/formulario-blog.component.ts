cc01e2e08cde4d2eda37a9b80e2917080a7a1d21
gtarrojo <175803244+gtarrojo@users.noreply.github.com>
2025-02-17 14:32:20 +0100

    create project

---
+++ src/app/components/formulario-blog/formulario-blog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-blog',
+  imports: [],
+  templateUrl: './formulario-blog.component.html',
+  styleUrl: './formulario-blog.component.css'
+})
+export class FormularioBlogComponent {
+
+}
---
+++ src/app/components/listado-blog/listado-blog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-blog',
+  imports: [],
+  templateUrl: './listado-blog.component.html',
+  styleUrl: './listado-blog.component.css'
+})
+export class ListadoBlogComponent {
+
+}
